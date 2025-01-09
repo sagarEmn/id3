43242ff82ce9693d24d0dfce3754fc84cc59c0bd
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-09 21:48:07 +0545

    feat(data): Add data

---
+++ src/components/data.js
@@ -0,0 +1,14 @@
+const P = "Present";
+const A = "Absent";
+const S = "Sick";
+const H = "Healthy";
+
+const data = [
+    [S, [P, P, A, A]],
+    [H, [A, A, P, P]],
+    [S, [P, A, P, A]],
+    [H, [P, P, P, A]],
+    [H, [A, P, A, P]],
+    [S, [A, P, A, A]],
+    [H, [P, A, P, P]]
+];
